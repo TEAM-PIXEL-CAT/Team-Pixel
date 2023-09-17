a227f3628667427faf94206a3813294e487c0510
Vinokurov Kamil <104242281+IWiseDreamI@users.noreply.github.com>
2023-09-17 16:37:31 +0300

    backend

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,21 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
+	i := new(int)
+	*i = 29
+
+	fmt.Println(*i)
+
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, !")
+		return c.SendString("Hello!")
 	})
 
 	app.Listen(":8000")
