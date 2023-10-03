7c7868f4f8320c0c1bd75b37e7d56a12a428cb04
Vinokurov Kamil <104242281+IWiseDreamI@users.noreply.github.com>
2023-10-03 15:21:56 +0300

    Fix duplicate

--- backend/teampixel/settings.py
+++ backend/teampixel/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.users.apps.UsersConfig',
     'apps.projects.apps.ProjectsConfig',
     'multiselectfield',
     'apps.users',
